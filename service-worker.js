784418dc1628bc000a51bcd4afe603f77e07f26f
callmemahdi01 <mr1451493@gmail.com>
2024-07-03 23:24:14 +0330

    pwa app for GPA

---
+++ service-worker.js
@@ -0,0 +1,31 @@
+const CACHE_NAME = 'gpa-calculator-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            })
+    );
+});
