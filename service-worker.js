ac82719e9c4ef6ed5dc6aa8ec5c640ae8c68ec43
callmemahdi01 <mr1451493@gmail.com>
2024-07-04 09:48:37 +0330

    update pwa

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,33 @@
-self.addEventListener('install', function(event) {
-    console.log('Service Worker installed');
-});
+const CACHE_NAME = 'gpa-cache-v1';
+const urlsToCache = [
+    '/GPA/',
+    '/GPA/index.html',
+    '/GPA/styles/main.css',
+    '/GPA/scripts/script.js',
+    '/GPA/icons/icon-192x192.png',
+    '/GPA/icons/icon-512x512.png',
+    '/GPA/manifest.json'
+];
 
-self.addEventListener('activate', function(event) {
-    console.log('Service Worker activated');
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(function(cache) {
+                console.log('Opened cache');
+                return cache.addAll(urlsToCache);
+            })
+    );
 });
 
 self.addEventListener('fetch', function(event) {
-    console.log('Fetching:', event.request.url);
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response) {
+                if (response) {
+                    return response;
+                }
+                return fetch(event.request);
+            }
+        )
+    );
 });
